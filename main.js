56ae7bed9814a509d8f70d7b4a394d4804795f5b
nicolascordoba1 <nicolascordoba97@hotmail.com>
2022-03-11 19:32:22 -0500

    Project completed and working

--- main.js
+++ main.js
@@ -6,41 +6,48 @@ const btn16 = document.querySelector('.button16')
 const btn32 = document.querySelector('.button32')
 const btn64 = document.querySelector('.button64')
 
-for(let i=0; i<256;i++){
-    const container = document.createElement('div')
-    container.classList.add('container')
-    mainContainer.appendChild(container)
-}
-draw()
+
 
 btn16.addEventListener('click', ()=>{
     mainContainer.innerHTML = '';
+    mainContainer.classList.remove('main-container64')
+    mainContainer.classList.remove('main-container32')
+    mainContainer.classList.add('main-container16')
+
     for(let i=0; i<256;i++){
         const container = document.createElement('div')
         container.classList.add('container')
         mainContainer.appendChild(container)
     }
     draw()  
-}, {once:true});
+});
 
 btn32.addEventListener('click', ()=>{
     mainContainer.innerHTML = '';
+    mainContainer.classList.remove('main-container64')
+    mainContainer.classList.remove('main-container16')
+    mainContainer.classList.add('main-container32')
+
     for(let i=0; i<1024;i++){
         const container = document.createElement('div')
         container.classList.add('container')
         mainContainer.appendChild(container)
     }
     draw()  
-}, {once:true});
+});
 btn64.addEventListener('click', ()=>{
     mainContainer.innerHTML = '';
+    mainContainer.classList.remove('main-container16')
+    mainContainer.classList.remove('main-container32')
+    mainContainer.classList.add('main-container64')
+
     for(let i=0; i<4096;i++){
         const container = document.createElement('div')
         container.classList.add('container')
         mainContainer.appendChild(container)
     }
     draw()  
-}, {once:true});
+});
 
 function draw(){
     const contenedor = document.querySelectorAll('.container')
